5e24b9b1eb6930332b3adabd24d508a9853b8fa4
Abhishek Mukherjee <amukherjee@dccil.com>
2026-04-14 17:46:34 +0530

    Updated week detail blade

--- resources/views/livewire/week-detail.blade.php
+++ resources/views/livewire/week-detail.blade.php
@@ -43,22 +43,4 @@
         :selected_day="$selected_day"
         :order_head="$order_head"
         route_name="order_measure_details" />
-
-    <!-- Legend -->
-    <section>
-        <div class="mb-5 d-flex gap-4 justify-content-center flex-wrap">
-            <div class="d-flex align-items-center gap-2">
-                <div style="background:#E8F8FF;border:1px solid #016B9D;width:30px;height:30px;border-radius:8px"></div>
-                <p class="mb-0">Not Started</p>
-            </div>
-            <div class="d-flex align-items-center gap-2">
-                <div style="background:#FFF9BC;border:1px solid #7A7000;width:30px;height:30px;border-radius:8px"></div>
-                <p class="mb-0">In Process</p>
-            </div>
-            <div class="d-flex align-items-center gap-2">
-                <div style="background:#CAFFB8;border:1px solid #208200;width:30px;height:30px;border-radius:8px"></div>
-                <p class="mb-0">Completed</p>
-            </div>
-        </div>
-    </section>
 </div>
